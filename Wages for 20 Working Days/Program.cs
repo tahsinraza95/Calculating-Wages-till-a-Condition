8c8abcdfe431fcbfdf94178da45e8635b3a30edd
tahsinraza95 <tahsinr693@gmail.com>
2023-02-01 09:29:04 +0530

    Added: Calculating Wahes Till a condition

---
+++ Wages for 20 Working Days/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Wages_for_20_Working_Days
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random= new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8; 
+                        break;
+                    default:
+                        empHrs = 0; 
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
